3c8d1354c257dd7c298d087bb42c56029592af23
Daniel Gibson Rocha Candeira do Carmo <danielgrccarmo@gmail.com>
2023-05-15 17:14:25 -0300

    Update atv4_Script.sql

--- atv4_Script.sql
+++ atv4_Script.sql
@@ -54,7 +54,7 @@ INSERT INTO medico_especialidade (id_medico, id_especialidade) VALUES
 
 /* Consulta 1:
 Todos os dados e o valor médio das consultas do ano de 2020 realizadas SEM convênio. */
-SELECT consulta.*,
+SELECT	consulta.*,
 	paciente.nome AS nome_paciente,
 	medico.nome AS nome_medico,
 	especialidade.especialidade AS nome_especialidade, (
@@ -69,15 +69,15 @@ ORDER BY data_consulta ASC;
 
 /*Consulta 2:
 Todos os dados e o valor médio das consultas do ano de 2020 realizadas POR convênio. */
-SELECT c.id AS consulta_id,
-    c.data_consulta, 
-    c.valor, 
-    p.nome AS nome_paciente, 
-    m.nome AS nome_medico, 
-    e.especialidade AS especialidade, 
-    cv.nome_convenio AS nome_convenio, 
-    cv.id AS convenio_id,
-    AVG(c.valor) OVER (PARTITION BY cv.id) AS valor_medio_consultas_por_convenio_2020 -- (para particionar linhas de tabela em grupos)
+SELECT	c.id AS consulta_id,
+	c.data_consulta,
+	c.valor,
+	p.nome AS nome_paciente,
+	m.nome AS nome_medico,
+	e.especialidade AS especialidade,
+	cv.nome_convenio AS nome_convenio,
+	cv.id AS convenio_id,
+	AVG(c.valor) OVER (PARTITION BY cv.id) AS valor_medio_consultas_por_convenio_2020 -- (para particionar linhas de tabela em grupos)
 FROM consulta c
 JOIN paciente p ON c.paciente_id = p.id
 JOIN medico m ON c.medico_id = m.id
@@ -92,7 +92,7 @@ SELECT * FROM internacao WHERE data_efetiva_alta > data_prevista_alta;
 
 /* Consulta 4:
 Receituário completo da primeira consulta registrada com receituário associado. */
-SELECT c.id AS 'Consulta',
+SELECT	c.id AS 'Consulta',
 	m.nome AS 'Médico',
 	p.nome AS 'Paciente',
 	r.medicamentos AS 'Medicamento'
@@ -113,23 +113,22 @@ UNION ALL -- > (distribui os resultados de várias instruções SELECT em uma ú
 SELECT c2.*
 FROM consulta c2
 WHERE c2.convenio_id IS NULL AND c2.valor = (
-    SELECT MIN(valor) FROM consulta WHERE convenio_id IS NULL
+	SELECT MIN(valor) FROM consulta WHERE convenio_id IS NULL
 );
 
 /* Consulta 6: 
 Todos os dados das internações em seus respectivos quartos, calculando o total da internação a partir do valor de diária do quarto e o número de dias entre a entrada e a alta. */
-SELECT 
-    i.id AS 'ID Internação',
-    p.nome AS 'Paciente',
-    m.nome AS 'Médico',
-    i.quarto_id AS 'ID do Quarto',
-    q.numero AS 'Número do Quarto',
-    t.descricao AS 'Tipo de Quarto',
-    i.data_entrada AS 'Data de Entrada',
-    i.data_prevista_alta AS 'Data Prevista de Alta',
-    i.data_efetiva_alta AS 'Data Efetiva de Alta',
-    DATEDIFF(i.data_efetiva_alta, i.data_entrada)  AS 'Dias de Internação',
-    i.procedimento AS 'Procedimento Realizado',
+SELECT	i.id AS 'ID Internação',
+	p.nome AS 'Paciente',
+	m.nome AS 'Médico',
+	i.quarto_id AS 'ID do Quarto',
+	q.numero AS 'Número do Quarto',
+	t.descricao AS 'Tipo de Quarto',
+	i.data_entrada AS 'Data de Entrada',
+	i.data_prevista_alta AS 'Data Prevista de Alta',
+	i.data_efetiva_alta AS 'Data Efetiva de Alta',
+	DATEDIFF(i.data_efetiva_alta, i.data_entrada)  AS 'Dias de Internação',
+	i.procedimento AS 'Procedimento Realizado',
 	(DATEDIFF(i.data_efetiva_alta, i.data_entrada) + 1) * t.valor_diaria AS 'Total da Internação'
 FROM internacao i
 INNER JOIN paciente p ON i.paciente_id = p.id
@@ -140,10 +139,9 @@ ORDER BY i.data_entrada;
 
 /* Consulta 7: 
 Data, procedimento e número de quarto de internações em quartos do tipo “apartamento”. */
-SELECT
-	i.data_entrada AS 'Data', 
-    i.procedimento AS 'Procedimento', 
-    q.numero AS 'Quarto'
+SELECT 	i.data_entrada AS 'Data',
+	i.procedimento AS 'Procedimento',
+	q.numero AS 'Quarto'
 FROM internacao i
 INNER JOIN quarto q ON i.quarto_id = q.id_quarto
 INNER JOIN tipo_quarto tq ON q.tipo_id = tq.id_tipo
@@ -152,10 +150,9 @@ WHERE tq.descricao = 'Apartamento';
 /* Consulta 8: 
 Nome do paciente, data da consulta e especialidade de todas as consultas em que os pacientes eram menores de 18 anos na data da consulta 
 e cuja especialidade não seja “pediatria”, ordenando por data de realização da consulta */
-SELECT
-	paciente.nome AS "Paciente",
-    consulta.data_consulta AS "Data da Consulta",
-    especialidade.especialidade AS "Especialidade"
+SELECT paciente.nome AS "Paciente",
+	consulta.data_consulta AS "Data da Consulta",
+	especialidade.especialidade AS "Especialidade"
 FROM consulta
 JOIN paciente ON consulta.paciente_id = paciente.id 
 JOIN especialidade ON consulta.especialidade_id = especialidade.id 
@@ -168,12 +165,12 @@ ORDER BY consulta.data_consulta;
 Nome do paciente, nome do médico, data da internação e procedimentos das internações realizadas por médicos da especialidade “gastroenterologia”, 
 que tenham acontecido em “enfermaria”. */
 SELECT
-	p.nome AS 'Paciente', 
-    m.nome AS 'Médico', 
-    i.data_entrada AS 'Internação',
-    i.procedimento AS 'Procedimento',
-    e.especialidade AS 'Especialidade',
-    tq.descricao AS 'Local'
+	p.nome AS 'Paciente',
+	m.nome AS 'Médico',
+	i.data_entrada AS 'Internação',
+	i.procedimento AS 'Procedimento',
+	e.especialidade AS 'Especialidade',
+	tq.descricao AS 'Local'
 FROM paciente p 
 JOIN internacao i ON p.id = i.paciente_id 
 JOIN medico m ON m.id = i.medico_id
@@ -186,11 +183,10 @@ ORDER BY i.data_entrada DESC;
 
 /* Consulta 10: 
 Os nomes dos médicos, seus CRMs e a quantidade de consultas que cada um realizou. */
-SELECT
-	m.nome AS 'Nome', 
-    m.crm AS 'CRM', 
-    COUNT(*) AS 'Quantidade de Consultas' -- The COUNT() function returns the number of records returned by a select query.
-FROM medico m 
+SELECT	m.nome AS 'Nome',
+	m.crm AS 'CRM',
+	COUNT(*) AS 'Quantidade de Consultas' -- The COUNT() function returns the number of records returned by a select query.
+FROM medico m
 JOIN consulta c ON c.medico_id = m.id 
 GROUP BY m.id;
 
@@ -208,10 +204,9 @@ HAVING COUNT(*) > 1;
 Retorna o nome do médico, a especialidade e a quantidade de consultas realizadas por cada médico.
 Exclui dados de médicos que não realizaram nenhuma consulta.
 Esta consulta envolve quatro tabelas: medico, especialidade, medico_especialidade e consulta */
-SELECT 
-	m.nome AS 'Médico', 
+SELECT	m.nome AS 'Médico',
 	e.especialidade AS 'Especialidade',
-    COUNT(c.id) AS 'Total de Consultas'
+	COUNT(c.id) AS 'Total de Consultas'
 FROM medico m
 JOIN medico_especialidade me ON m.id = me.id_medico
 JOIN especialidade e ON me.id_especialidade = e.id
